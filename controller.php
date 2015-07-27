060f7749035a676a3190cb0b7654a6c27d40fd74
dorian-marchal <marchal.dorian@gmail.com>
2015-07-27 19:06:31 +0200

    Add Kint

---
+++ controller.php
@@ -0,0 +1,29 @@
+<?php
+namespace Concrete\Package\DebugKit;
+
+use Concrete\Core\Package\Package;
+use View;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class Controller extends Package {
+    protected $pkgHandle = 'debug_kit';
+    protected $appVersionRequired = '5.7';
+    protected $pkgVersion = '0.1.0';
+
+    public function getPackageDescription() {
+        return t('Add Kint debugging tools');
+    }
+
+    public function getPackageName() {
+        return t('Debug Kit');
+    }
+
+    public function on_start() {
+        $al = \Concrete\Core\Asset\AssetList::getInstance();
+        $al->register('css', 'debug', '../packages/debug/css/debug.css');
+        View::getInstance()->requireAsset('css', 'debug');
+    }
+}
