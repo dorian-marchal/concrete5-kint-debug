a05ba86f3023c9fd93492db16963d31da63628c2
Dorian MARCHAL <marchal.dorian@gmail.com>
2015-07-28 12:17:22 +0200

    Fix CSS getting required in AJAX request

--- controller.php
+++ controller.php
@@ -4,7 +4,7 @@ namespace Concrete\Package\KintDebug;
 use Concrete\Core\Package\Package;
 use View;
 
-defined('C5_EXECUTE') or die(t("Access Denied."));
+defined('C5_EXECUTE') or die(_("Access Denied."));
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -23,7 +23,21 @@ class Controller extends Package {
 
     public function on_start() {
         $al = \Concrete\Core\Asset\AssetList::getInstance();
-        $al->register('css', $this->pkgHandle . '/css', 'css/debug.css', array(), $this->pkgHandle);
-        View::getInstance()->requireAsset('css', $this->pkgHandle . '/css');
+        $al->register('css', 'kint_debug/css', 'css/debug.css', array(), $this->pkgHandle);
+
+        // Never include the css in ajax responses
+        if(!$this->isAjaxRequest()) {
+            View::getInstance()->requireAsset('css', 'kint_debug/css');
+        }
+    }
+
+    /**
+     * Check, if possible, that this execution was triggered by an AJAX request.
+     * @return bool
+     */
+    protected function isAjaxRequest()
+    {
+        return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+            strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
     }
 }
